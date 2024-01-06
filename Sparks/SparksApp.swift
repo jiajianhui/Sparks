215d8ff2397d10e3635b6b057c2cc382cf59001e
jiajianhui <jia15176168273@icloud.com>
2024-01-06 15:25:53 +0800

    Initial Commit

---
+++ Sparks/SparksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparksApp.swift
+//  Sparks
+//
+//  Created by 贾建辉 on 2024/1/6.
+//
+
+import SwiftUI
+
+@main
+struct SparksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
