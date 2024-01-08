d74acdf5138fadbfce981f0abffe1419f5e2b409
jiajianhui <jia15176168273@icloud.com>
2024-01-08 21:10:48 +0800

    新增FaceID解锁功能

--- Sparks/SparksApp.swift
+++ Sparks/SparksApp.swift
@@ -6,26 +6,67 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 @main
 struct SparksApp: App {
+    
+    @AppStorage("isToggle") var isToggle = false //将变量存储起来
+    @State var locked = true
+    
     var body: some Scene {
         WindowGroup {
-            TabView {
-                ListView()
-                    .environment(\.managedObjectContext, CoreDataManager.shared.viewContext)
-                    .tabItem {
-                        Image(systemName: "bubbles.and.sparkles")
-                        Text("灵感")
+            
+            Group {
+                if locked && isToggle {
+                    LockedView(authorize: authorize)
+                } else {
+                    TabView {
+                        ListView()
+                            .environment(\.managedObjectContext, CoreDataManager.shared.viewContext)
+                            .tabItem {
+                                Image(systemName: "bubbles.and.sparkles")
+                                Text("灵感")
+                            }
+                        
+                        SettingView(isToggle: $isToggle)
+                            .tabItem {
+                                Image(systemName: "gear")
+                                Text("设置")
+                            }
                     }
-                
-                SettingView(isToggle: .constant(false))
-                    .tabItem {
-                        Image(systemName: "gear")
-                        Text("设置")
-                    }
+                }
+            }
+            .onAppear {
+                if isToggle {
+                    authorize()
+                }
+            }
+            .onChange(of: isToggle) { newValue in
+                if newValue {
+                    authorize()
+                }
             }
             
         }
+    }
+}
+
+
+extension SparksApp {
+    func authorize() {
+        let context = LAContext()  //开发者与框架的中介
+        var error: NSError?  //框架要求的错误类型
+        
+        //1、判断设备是否支持FaceID或TouchID
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // 2、识别函数
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "解锁应用") { result, _ in
+                if result {
+                    locked = false
+                }
+            }
+        }
+        
     }
 }
---
+++ Sparks/View/LockedView.swift
@@ -0,0 +1,39 @@
+//
+//  LockedView.swift
+//  Sparks
+//
+//  Created by 贾建辉 on 2024/1/8.
+//
+
+import SwiftUI
+
+struct LockedView: View {
+    var authorize: () -> ()  //函数类型，无参数和返回值
+    
+    var body: some View {
+        Button {
+            authorize()
+        } label: {
+            VStack {
+                Image(systemName: "faceid")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, height: 60)
+                    .padding(.bottom, 4)
+                Text("点击解锁")
+            }
+            .foregroundColor(.primary.opacity(0.4))
+            .offset(y: -30)
+        }
+    }
+}
+
+struct LockedView_Previews: PreviewProvider {
+    static var previews: some View {
+        LockedView(authorize: test)
+    }
+    
+    static func test() {
+        print("hello")
+    }
+}
